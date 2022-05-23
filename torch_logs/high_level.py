220bead14dd12bddcee127671e7195b1d395598d
yohan-pg <pg.yohan@gmail.com>
2022-05-22 21:47:11 -0400

    added many logging features

--- torch_logs/high_level.py
+++ torch_logs/high_level.py
@@ -4,55 +4,95 @@ from .low_level import *
 from .utils import tee_output
 
 
-class Event(enum.Enum):
+class LogEvent(enum.Enum):
     INIT = "INIT"
     PROGRESS = "PROGRESS"
     CHECKPOINT = "CHECKPOINT"
 
 
-def simple_logs(
-    name: str,
-    iteration: int,
+Evaluation = Callable[[], Iterable[tuple[torch.Tensor, pd.DataFrame]]]
+
+
+def simple_logging(
     model: nn.Module,
-    losses: pd.DataFrame,
-    progress_freq: int = 1_000,
-    checkpoint_freq: int = 10_000,
-) -> set[Event]:
-    if os.path.exists(name):
-        events = set()
-    else:
-        if "pytest" not in sys.argv[0]: # todo figure out why this fails in tests
-            tee_output("out.log")
-        events = set([Event.INIT])
+    *,
+    name: str,
+    evaluation: Optional[Evaluation],
+    comment: Optional[str] = None,
+    progress_freq: int = 200,
+    checkpoint_freq: int = 200,
+    heartbeat_freq: int = 20,
+    window_size: int = 1,
+) -> Callable[[int, pd.DataFrame], Iterable[LogEvent]]:
+    def logging_step(
+        iteration: int,
+        losses: pd.DataFrame,
+    ) -> Iterable[LogEvent]:
+        with directory(name) as exists_already:
+            if not exists_already:
+                init_logging(name, model, comment)
+                yield LogEvent.INIT
 
-    with experiment(name):
-        if iteration % progress_freq == 0:
-            log_progress(losses)
-            events.add(Event.PROGRESS)
+            if iteration % progress_freq == 0:
+                yield progress_logging(losses, window_size)
 
-        if iteration % checkpoint_freq == 0:
-            with directory(f"{iteration//checkpoint_freq:06d}"):
-                log_checkpoint(model)
-                events.add(Event.CHECKPOINT)
+            if iteration % checkpoint_freq == 0:
+                with directory(
+                    f"checkpoints/{iteration//checkpoint_freq:05d}"
+                ):  # todo move inside, tricky
+                    yield checkpoint_logging(model, evaluation)
 
-    return events
+            if iteration % heartbeat_freq == 0:
+                heartbeat_logging()
 
-
-@contextlib.contextmanager
-def experiment(directory_path: str):
-    with directory(directory_path):
-        log_pid()
-        log_config()
-        yield
+    return logging_step
 
 
-def log_progress(losses) -> None:
+def init_logging(name: str, model: nn.Module, comment: Optional[str]) -> LogEvent:
+    if "pytest" not in sys.argv[0]: # todo figure out why this fails in tests
+        tee_output("out.log")
+
+    log_pid()
+    log_config()
+    log_max_memory()
+
+    log_architecture(model)
+
+    if comment is not None:
+        log_comment(comment)
+
+    create_symbolic_link(name)
+
+    return LogEvent.INIT
+
+
+def progress_logging(losses: pd.DataFrame, window_size: int) -> LogEvent:
+    losses = losses.rolling(window_size).mean().iloc[::window_size, :][1:]
+    # Some windowing operations also support an online method after constructing a windowing object which returns a new object that supports passing in new DataFrame or Series objects to continue the windowing calculation with the new values (i.e. online calculations).
+
     log_losses(losses)
-    plot_losses(losses)
+    plot_losses(window_size)
+
+    return LogEvent.PROGRESS
 
 
-def log_checkpoint(model) -> None:
+def checkpoint_logging(model: nn.Module, evaluation: Evaluation) -> LogEvent:
+    if evaluation is not None:
+        for j, (preds, scores) in enumerate(evaluation()):
+            with directory("predictions"):
+                log_predictions(j, preds)
+
+        with directory("../.."):
+            log_scores(scores)
+            plot_scores()
+
     log_model(model)
     log_weights(model)
 
+    return LogEvent.CHECKPOINT
+
+
+def heartbeat_logging():
+    log_last_modified()
+    log_max_memory()
 
--- torch_logs/low_level.py
+++ torch_logs/low_level.py
@@ -1,22 +1,61 @@
 from .imports import *
 
+# todo this should be a class se we can override?
+
 
 @contextlib.contextmanager
-def directory(path: str):
-    if not os.path.exists(path):
+def directory(path: str) -> Iterable[bool]:
+    exists_already = os.path.exists(path)
+    if not exists_already:
         os.makedirs(path)
+
     cwd = os.getcwd()
     os.chdir(path)
+
     try:
-        yield
+        yield exists_already
     finally:
         os.chdir(cwd)
+
+
+def log_comment(comment: str):
+    latest_comment_path = "../latest/comment.txt"
+    if os.path.exists("../latest/comment.txt"):
+        with open(latest_comment_path, "r") as f:
+            assert (
+                f.read() != comment
+            ), "Comment did not change between runs! Write a new comment or delete the old one."
+
+    with open("comment.txt", "w") as f:
+        f.write(comment)
+
+
+def log_architecture(model: nn.Module):
+    with open("architecture.txt", "w") as f:
+        f.write(str(model))
 
 
 def log_pid() -> None:
     with open("pid.txt", "w") as f:
         f.write(str(os.getpid()))
-        
+
+
+def log_max_memory() -> None:
+    with open("max_memory.txt", "w") as f:
+        f.write(
+            str(torch.cuda.max_memory_allocated())
+        )  # todo generalize to multiple devices
+
+
+def log_last_modified() -> None:
+    with open("last_modified.txt", "w") as f:
+        f.write(
+            datetime.now()
+            .isoformat(timespec="seconds")
+            .replace(":", "")
+            .replace("-", "")
+        )
+
 
 def log_config() -> None:
     shutil.copy(main.__file__, "config.py")
@@ -34,7 +73,80 @@ def log_model(model: nn.Module) -> None:
     torch.save(model, "model.pt")
 
 
-def plot_losses(losses: pd.DataFrame) -> None:
-    fig = px.line(losses, title="Losses over time", log_y=True)
-    fig.write_image("plot.png")
-    fig.write_html("plot.html")
+def plot_losses(window_size: int) -> None:
+    # todo losses from the .csv file
+    losses = pd.read_csv("losses.csv")
+    os.makedirs("plots", exist_ok=True)
+
+    images = []
+
+    colors = px.colors.qualitative.Plotly
+
+    for col in losses.columns:
+        fig = px.line(
+            losses[col],
+            title=f"Total weighted objective per iteration (window size {window_size})"
+            if col == "objective"
+            else f"Average '{col}' loss per iteration (window size {window_size})",
+            y=col,
+            log_y=True,
+            template="plotly_dark",
+            color_discrete_sequence=colors[1:] if col == "objective" else colors,
+        ).update_layout(showlegend=False, xaxis_title="iteration")
+        fig.write_image(f"plots/losses_{col}.png")
+        fig.write_html(f"plots/losses_{col}.html")
+        images.append(read_image(f"plots/losses_{col}.png"))
+
+    save_image(torch.stack(images) / 255, "losses.png", padding=0, nrow=1)
+
+
+def plot_scores() -> None:
+    scores = pd.read_csv("scores.csv")
+    os.makedirs("plots", exist_ok=True)
+
+    images = []
+
+    for col in scores.columns:
+        xs = list(range(1, len(scores[col]) + 1))
+        fig = (
+            px.line(
+                scores[col],
+                x=xs,
+                y=col,
+                title=f"'{col}' score per checkpoint",
+                log_y=True,
+                template="plotly_dark",
+            )
+            .update_traces(mode="lines+markers")
+            .update_layout(showlegend=False, xaxis_title="checkpoint")
+            .update_xaxes(tickmode="array", tickvals=xs)
+        )
+
+        fig.write_image(f"plots/scores_{col}.png")
+        fig.write_html(f"plots/scores_{col}.html")
+
+        images.append(read_image(f"plots/scores_{col}.png"))
+
+    save_image(torch.stack(images) / 255, "scores.png", padding=0, nrow=1)
+
+
+def log_scores(scores: pd.DataFrame) -> None:
+    scores.mean().to_frame().T.to_csv(
+        "scores.csv", mode="a", index=False, header=not os.path.exists("scores.csv")
+    )
+
+
+def log_predictions(j: int, preds: torch.Tensor):
+    if preds.ndim == 4:
+        for k, image in enumerate(preds):
+            name = f"{j*preds.shape[0]+k:06d}"
+            directory = name[:4]
+            os.makedirs(directory, exist_ok=True)
+            save_image(image, f"{directory}/{name}.png")
+
+
+def create_symbolic_link(name: str):
+    with directory(".."):
+        if os.path.exists("latest"):
+            os.unlink("latest")
+        os.symlink(name.split("/")[-1], "latest")
